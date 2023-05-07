08f50b9a4fed79c3f6f619a4b6848997fd72d3e5
laoluobo <luoyu0621@outlook.com>
2023-05-07 23:08:00 +0800

    add sql

--- src/WMS.MySQL.Repository/WMSDB.cs
+++ src/WMS.MySQL.Repository/WMSDB.cs
@@ -10,12 +10,13 @@ namespace WMS.MySQL.Repository
         public WMSDB(DbContextOptions<WMSDB> options) : base(options)
         {
         }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.UseCollation("utf8mb4_0900_ai_ci")
+                        .HasCharSet("utf8mb4");
             modelBuilder.Entity<UserInfo>()
                  .Property(o => o.CreateTime).HasDefaultValue(DateTime.Now);
-
         }
-
     }
 }
