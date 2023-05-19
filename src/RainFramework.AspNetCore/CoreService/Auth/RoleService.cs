1660b8cab984acdb653003ca4c8986bbab0a1c3b
IversonXia <Iverson_Xia@sgssemi.com>
2023-05-19 09:35:19 +0800

    名称对应

--- src/RainFramework.AspNetCore/CoreService/Auth/RoleService.cs
+++ src/RainFramework.AspNetCore/CoreService/Auth/RoleService.cs
@@ -21,11 +21,11 @@ namespace RainFramework.AspNetCore.CoreService.Auth
 
         public async Task<bool> DeleteRoleById(int id)
         {
-            await dbContext.Roles.Where(menu => menu.Id == id).ExecuteDeleteAsync();
+            await dbContext.Roles.Where(role => role.Id == id).ExecuteDeleteAsync();
             var count = await dbContext.SaveChangesAsync() > 0;
             if (!count)
             {
-                throw new NotFoundException($"The menus id is {id} not found!");
+                throw new NotFoundException($"The roles id is {id} not found!");
             }
             return count;
         }
