1d0093b3c9aa7ccdc115fcc44fd69a1924bff6a9
Andy <luoyu0621@outlook.com>
2023-05-30 16:28:26 +0800

    自动加载 AutoMaperProfile

--- src/RainFramework.AspNetCore/ServiceProvider.cs
+++ src/RainFramework.AspNetCore/ServiceProvider.cs
@@ -1,11 +1,12 @@
-﻿using System.Security.Claims;
+﻿using System.Reflection;
+using System.Security.Claims;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RainFramework.AspNetCore.CoreService.Auth;
-using RainFramework.AspNetCore.Mapper;
 using RainFramework.Common.Configurer;
 using RainFramework.Repository;
 using Serilog;
@@ -19,7 +20,7 @@ namespace RainFramework.AspNetCore
         {
             builder.Services.AddMyCors();
             builder.Host.UseSerilogger();
-            builder.Services.AddSwagger();          
+            builder.Services.AddSwagger();
             builder.Services.AddJwtBearerPkg();
             builder.Services.AddSingleton<IJWTService, JWTService>();
             builder.Services.AddBaseDBContext(builder.Configuration.GetConnectionString("MySql"));
@@ -27,24 +28,25 @@ namespace RainFramework.AspNetCore
             builder.Services.AddTransient<IUserInfoService, UserInfoService>();
             builder.Services.AddTransient<IMenuService, MenuService>();
             builder.Services.AddTransient<IRoleService, RoleService>();
-            
-            builder.Services.AddAutoMapper(typeof(CoreProfile));
+
+            var profiles = Assembly.GetExecutingAssembly().GetTypes().Where(t => t.BaseType == typeof(Profile)).ToArray();
+            builder.Services.AddAutoMapper(profiles);
 
             application = builder.Build();
             //启用跨域请求
             application.UseCors();
-           application.UseSerilogRequestLogging(option =>
-            {
-                option.MessageTemplate = "User {UserName} ClientIp {ClientIp} " + option.MessageTemplate;
+            application.UseSerilogRequestLogging(option =>
+             {
+                 option.MessageTemplate = "User {UserName} ClientIp {ClientIp} " + option.MessageTemplate;
 
-                option.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Debug;
+                 option.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Debug;
 
-                option.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
-                {
-                    diagnosticContext.Set("UserName", httpContext.User.Claims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value ?? "None");
-                    diagnosticContext.Set("ClientIp", httpContext.Connection.RemoteIpAddress?.MapToIPv4());
-                };
-            });
+                 option.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+                 {
+                     diagnosticContext.Set("UserName", httpContext.User.Claims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value ?? "None");
+                     diagnosticContext.Set("ClientIp", httpContext.Connection.RemoteIpAddress?.MapToIPv4());
+                 };
+             });
 
             if (application.Environment.IsDevelopment())
             {
