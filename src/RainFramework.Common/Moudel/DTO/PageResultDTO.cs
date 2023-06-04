103e34402224421e15efc82588888699a7b6b396
Andy <luoyu0621@outlook.com>
2023-06-04 16:30:05 +0800

    1

---
+++ src/RainFramework.Common/Moudel/DTO/PageResultDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RainFramework.Common.Moudel.DTO
+{
+    /// <summary>
+    /// 分页
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public record PageResultDTO<T>(List<T>? Datas, int? Count);
+}
