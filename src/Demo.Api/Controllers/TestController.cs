d1c333b3dd9c1e8c39a447ac9dca89b4372b0f18
laoluobo <luoyu0621@outlook.com>
2023-06-22 12:25:19 +0800

    1

---
+++ src/Demo.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController:ControllerBase
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+}
