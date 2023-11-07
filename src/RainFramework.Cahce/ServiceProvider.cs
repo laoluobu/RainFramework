36ab6cbf598f17b38a95c75ec0fd2a0e94c1dbce
Andy_Luo <Andy_Luo@sgssime.com>
2023-11-07 09:56:52 +0800

    1

--- src/RainFramework.Cahce/ServiceProvider.cs
+++ src/RainFramework.Cahce/ServiceProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
 
 namespace RainFramework.Cahce
 {
@@ -6,6 +7,7 @@ namespace RainFramework.Cahce
     {
         public static IServiceCollection AddRFMemoryCache(this IServiceCollection services, int sizeLimit)
         {
+            Debug.WriteLine($"[AddService] RFMemoryCache sizeLimit={sizeLimit}");
             return services.AddMemoryCache(option =>
             {
                 if (sizeLimit > 0)
