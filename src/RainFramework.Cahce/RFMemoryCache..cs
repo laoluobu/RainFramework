8161421eec11d353b410520be362963f3a10b051
Andy_Luo <Andy_Luo@sgssime.com>
2023-11-07 11:27:24 +0800

    RF Cache add Queue

--- src/RainFramework.Cahce/RFMemoryCache..cs
+++ src/RainFramework.Cahce/RFMemoryCache..cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
+using System.Collections.Concurrent;
 
 namespace RainFramework.Cahce
 {
     public class RFMemoryCache
     {
         private readonly IMemoryCache memoryCache;
-      
+
         private readonly object hashSetLock = new object();
+
 
         public RFMemoryCache(IMemoryCache memoryCache)
         {
@@ -44,7 +46,7 @@ namespace RainFramework.Cahce
             if (expiration != null)
             {
                 cacheEntryOptions.SetAbsoluteExpiration((TimeSpan)expiration);
-            }     
+            }
             memoryCache.Set(key, value, cacheEntryOptions);
         }
 
@@ -76,8 +78,8 @@ namespace RainFramework.Cahce
         /// </summary>
         /// <param name="key"></param>
         /// <param name="value"></param>
-        /// <param name="expiration">整个列表的过期时间，每次修改都会重置</param>
-        public void PushHashSet<T>(string key, T value, TimeSpan? expiration)
+        /// <param name="expiration">整个列表的过期时间，每次修改都会重置,null 则不会失效</param>
+        public void PushValueToHashSet<T>(string key, T value, TimeSpan? expiration)
         {
             lock (hashSetLock)
             {
@@ -92,17 +94,12 @@ namespace RainFramework.Cahce
             }
         }
 
-        private void CreateHashSet<T>(string key, HashSet<T> set, TimeSpan? expiration)
-        {
-            Set(key + "HashSet", set, expiration, 10);
-        }
-
         /// <summary>
         /// 从hashSet移除某个值
         /// </summary>
         /// <param name="key"></param>
         /// <param name="value"></param>
-        public void RemoveValueHashSet<T>(string key, T value)
+        public void RemoveValueFromHashSet<T>(string key, T value)
         {
             lock (hashSetLock)
             {
@@ -138,9 +135,44 @@ namespace RainFramework.Cahce
         {
             lock (hashSetLock)
             {
-                return FindHashSet<T>(key);
+                return FindHashSet<T>(key + "HashSet");
             }
         }
+
+
+        /// <summary>
+        /// 加入Queue或者创建Queue
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="item"></param>
+        /// <param name="expiration"></param>
+        public void Enqueue<T>(string key, T item, TimeSpan? expiration)
+        {
+            var queue = FindQueue<T>(key);
+            if (queue != null)
+            {
+                queue.Enqueue(item);
+                Set(key, queue, expiration);
+            }
+            else
+            {
+                var newQueue = new ConcurrentQueue<T>();
+                newQueue.Enqueue(item);
+                CreateQueue(key, newQueue, expiration);
+            }
+        }
+
+        public T? Dequeue<T>(string key)
+        {
+            var queue = FindQueue<T>(key);
+            if(queue?.TryDequeue(out var value) ?? false)
+            {
+                return value;
+            }
+            return default;
+        }
+
 
         private HashSet<T>? FindHashSet<T>(string key)
         {
@@ -151,5 +183,26 @@ namespace RainFramework.Cahce
             }
             return null;
         }
+
+        public ConcurrentQueue<T>? FindQueue<T>(string key)
+        {
+            bool isFound = memoryCache.TryGetValue(key + "Queue", out ConcurrentQueue<T>? queue);
+            if (isFound)
+            {
+                return queue;
+            }
+            return null;
+        }
+
+        private void CreateHashSet<T>(string key, HashSet<T> set, TimeSpan? expiration)
+        {
+            Set(key + "HashSet", set, expiration, 10);
+        }
+
+        private void CreateQueue<T>(string key, ConcurrentQueue<T> queue, TimeSpan? expiration)
+        {
+            Set(key + "Queue", queue, expiration, 10);
+        }
+
     }
 }
