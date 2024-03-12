5c29f5ae99fb2a5f5bb55c0fcadcda8f6f3423b9
Andy <Andy_Luo@sgssime.com>
2024-03-12 13:41:09 +0800

    feat: Dialog

---
+++ src/RainWPF/Controls/Dialog.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace RainWPF.Controls
+{
+    public class Dialog : Window
+    {
+        public Dialog()
+        {
+            WindowStyle = WindowStyle.ToolWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            ShowInTaskbar = false;
+        }
+
+        public static readonly DependencyProperty RainDialogResultProperty = DependencyProperty.RegisterAttached("RainDialogResult", typeof(bool?), typeof(Dialog), new PropertyMetadata(DialogResultChanged));
+
+        private static void DialogResultChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Window window)
+            {
+                window.DialogResult = e.NewValue as bool?;
+            }
+        }
+
+        public static void SetDialogResult(Window target, bool? value)
+        {
+            target.SetValue(RainDialogResultProperty, value);
+        }
+    }
+}
