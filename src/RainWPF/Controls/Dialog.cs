d763ef4fa045cb4a6ecc8e8b3cfdd48dcfd4d074
Andy <Andy_Luo@sgssime.com>
2024-03-12 13:48:44 +0800

    delete: 无效属性

--- src/RainWPF/Controls/Dialog.cs
+++ src/RainWPF/Controls/Dialog.cs
@@ -10,20 +10,5 @@ namespace RainWPF.Controls
             WindowStartupLocation = WindowStartupLocation.CenterOwner;
             ShowInTaskbar = false;
         }
-
-        public static readonly DependencyProperty RainDialogResultProperty = DependencyProperty.RegisterAttached("RainDialogResult", typeof(bool?), typeof(Dialog), new PropertyMetadata(DialogResultChanged));
-
-        private static void DialogResultChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is Window window)
-            {
-                window.DialogResult = e.NewValue as bool?;
-            }
-        }
-
-        public static void SetDialogResult(Window target, bool? value)
-        {
-            target.SetValue(RainDialogResultProperty, value);
-        }
     }
 }
