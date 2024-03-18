97b8d8ba9fa9959140c085aca6d6172c3b876f7a
Andy <Andy_Luo@sgssime.com>
2024-03-18 11:58:54 +0800

    WIP

---
+++ src/RainDesktop/ViewModel/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RainDesktop.ViewModel
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+
+    public partial class DialogVMBase : ViewModelBase
+    {
+        [ObservableProperty]
+        private bool? dialogResult;
+    }
+}
